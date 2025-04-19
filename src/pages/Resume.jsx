792d8978221eebf98acf129baa86d719676d0693
Sherif-Sakran <Sherif-Sakran>
2025-04-19 18:40:33 +0100

    added the resume structure and style as components. some content is left.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import '../css/Resume.css'
+import ResumeBody from '../components/ResumeBody'
 
 const Resume = () => {
   return (
-    <div>
-      <h1>Resume</h1>
-    </div>
+    <>
+      <div className="resume-container">
+        <div className="resume-header">
+        <h1 className="resume-title">Resume</h1> 
+        <div className="text-center">
+          <a href="/resume.pdf" download className="resume-download">
+            Download PDF
+          </a>
+          </div>
+        </div>
+        <p className="resume-subtitle">A quick overview of my education, experience, and achievements.</p>
+        
+        <ResumeBody />
+      </div>
+    </>
   )
 }
 
