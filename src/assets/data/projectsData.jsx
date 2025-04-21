79d3569adf36a326b11e471b481dc222b3d54609
Sherif-Sakran <Sherif-Sakran>
2025-04-21 15:39:10 +0100

    added a project

--- src/assets/data/projectsData.jsx
+++ src/assets/data/projectsData.jsx
@@ -27,6 +27,26 @@ export const projects = [
     live: "",
     report: ""
   },
+  {
+    id: "face-recognition",
+    title: "Face Recognition",
+    type: " Group Project",
+    description: "A desktop application for face recognition and image metadata recording.",
+    image: "../../public/screenshots/face-recognition/0 home.PNG",
+    tech: ["Python", "face-recognition", "Tkinter"], 
+    details: "This project was developed during my internship at AUC Digital Libraries. I and my teammate, Andrew Hany, developed this desktop application as a tool AUC Libraries to digitise its assets. We had implemented three required functionalities: training a machine learning model on people’s faces in a given set of photos; recognising all known faces in all photos in a given directory; and storing the metadata of the photos locally. We used Python's face-recognition library provides a pre-trained model for face recognition, which is based on deep learninig techniques (mainly the few shot learning). The library uses a convolutional neural network (CNN) to extract facial features from images in the form of encodings (i.e., embeddings) and then compares these features to identify faces in new images by applying a similarity metric like cosine similarity. We used Tkinter to build the GUI for the application, which allows users to interact with the system easily. The application is designed to be user-friendly and efficient, making it suitable for libraries and other institutions that need to manage large collections of images.",
+
+
+    notes: "Thanks to our mentor Mr. Muehlhaeusler, we had the opportunity to share this application in the Middle East Librarians Association (MELA) conference in 2022 as a tool that can help libraries make use of its assets.",
+
+    galleryData:[{imagePath: "../../public/screenshots/face-recognition/1 tagging.jpg",caption: "The application detects the faces of AUC's Ex-Pres. Riccardone and Pres. Dallal but do not recognise them. This is the tagging step in which users fills in the metadata of the image stores the cropped faces for generating embeddings. "},{imagePath: "../../public/screenshots/face-recognition/2 recognition.png", caption: "The application detects and recognises the face of Sir. Magdi Yaacoup from the photo."},{imagePath: "../../public/screenshots/face-recognition/4 environment creation.jpg",caption: "The environment creation window that automatically creates the environment for training the faces and storing both the embeddings of the faces and the metadata of the photos."},{imagePath: "../../public/screenshots/face-recognition/3 metadata.png",caption: "The metadata table for the tagged images."},{imagePath: "../../public/screenshots/face-recognition/5 testing suite.png",caption: "The testing suite of one of the tests done on individual photos."},{imagePath: "../../public/screenshots/face-recognition/6 presentation.jpg",caption: "A live photo from the conference where our mentor virtually represents AUC Libraries where our project is one of the two participating projects from AUC."}, ],
+    github: "https://github.com/Sherif-Sakran/face_recognition ",
+    live: "",
+    report: ""
+  },
+
+
+
 
 
   ];
